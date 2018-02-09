307647af472c8eaaba3cdac0cf983b7d68dda7cb
dev <dev@dev-LIFEBOOK-AH531>
2018-02-09 14:31:40 +0100

    makefile update

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -35,7 +35,13 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/main.o
+	${OBJECTDIR}/cleaner.o \
+	${OBJECTDIR}/counters.o \
+	${OBJECTDIR}/cpu.o \
+	${OBJECTDIR}/event_alarm.o \
+	${OBJECTDIR}/fork.o \
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/monitor.o
 
 
 # C Compiler Flags
@@ -62,10 +68,40 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/perf-test: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/perf-test ${OBJECTFILES} ${LDLIBSOPTIONS}
 
+${OBJECTDIR}/cleaner.o: cleaner.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Iinclude -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cleaner.o cleaner.cpp
+
+${OBJECTDIR}/counters.o: counters.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Iinclude -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/counters.o counters.cpp
+
+${OBJECTDIR}/cpu.o: cpu.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Iinclude -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cpu.o cpu.cpp
+
+${OBJECTDIR}/event_alarm.o: event_alarm.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Iinclude -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/event_alarm.o event_alarm.cpp
+
+${OBJECTDIR}/fork.o: fork.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Iinclude -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/fork.o fork.cpp
+
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -Iinclude -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+
+${OBJECTDIR}/monitor.o: monitor.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Iinclude -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/monitor.o monitor.cpp
 
 # Subprojects
 .build-subprojects:
