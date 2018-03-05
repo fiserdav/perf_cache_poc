4a0575ccc643d254ae2be53dd4480649166fc809
fiserdav <36033283+fiserdav@users.noreply.github.com>
2018-03-05 10:37:37 +0100

    Update event_alarm.cpp

--- event_alarm.cpp
+++ event_alarm.cpp
@@ -5,17 +5,11 @@ using namespace std;
 
 void eventAlarm(float_t value, struct event_ref * event) {
     time_t tm = time(NULL);
-    if (value > ERR_TRESHOLD) {
+    if (value > ALARM_TRESHOLD) {
         cout << tm << "; " << "ERR: " << value << "\t" << event->cpu << "\t" << event->pid << "\t" << event->tid << "\t" 
                 << event->counters.couter_values[0].value << "\t" << event->counters.couter_values[1].value << endl;
 
-    } else if (value > WARN_TRESHOLD) {
-         cout << tm << "; " << "WRN: " << value << "\t" << event->cpu << "\t" << event->pid << "\t" << event->tid << "\t" 
-                 << event->counters.couter_values[0].value << "\t" << event->counters.couter_values[1].value << endl;
-    } else if (value > INFO_TRESHOLD) {
-          cout << tm << "; " << "INF: " << value << "\t" << event->cpu << "\t" << event->pid << "\t" << event->tid << "\t" 
-                  << event->counters.couter_values[0].value << "\t" << event->counters.couter_values[1].value << endl;
-    }
+    } 
     //cout << "pid: " << event->pid << "; tid: " << event->tid << "; cpu: " << event->cpu << endl;
 
 }
